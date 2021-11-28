dac4806dc4b3d341cc76659091d14cc39ed85404
wenwen <66482267@qq.com>
2021-11-28 23:37:01 +0800

    增加app文件

---
+++ www/app.py
@@ -0,0 +1,8 @@
+import logging; logging.basicConfig(level=logging.INFO)
+import asyncio, os, json, time
+from datetime import datetime
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome<h1>')
+
